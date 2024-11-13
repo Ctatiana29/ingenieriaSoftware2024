29b85d7dbda60a8e7f56da4bccdc54b49915ef2a
Ctatiana29 <tatiana.caceres@est.ulsa.edu.ni>
2024-11-12 20:49:07 -0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,25 @@
+from flask import Flask, jsonify, request
+
+app=Flask(__name__)
+#endpoint raiz
+@app.route("/")
+def root():
+    return "Hola mundo"
+@app.route("/users/<user_id>")
+def get_user(user_id):
+    user={
+        "id":user_id,
+        "name":"Aarock",
+        "telefono":"57993449"
+    }
+    query=request.args.get("query")
+    if query:
+        user["query"]=query
+    return jsonify(user),200
+@app.route('/users',methods=['POST'])
+def create_user():
+    data=request.get_json()
+    data["status"]="user created"
+    return jsonify(data),201
+if __name__=='__main__':
+    app.run(debug=True)
